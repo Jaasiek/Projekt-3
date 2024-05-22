50f39c9b19c3c8b7acbbd5136a924b17913df61a
Jasiek <u119_jangaw_waw@technischools.com>
2024-05-22 11:31:00 +0200

    upload all first

---
+++ script.js
@@ -0,0 +1,22 @@
+const cards = [
+  { id: 1, src: "/assets/adrian.webp" },
+  { id: 2, src: "/assets/darek.webp" },
+  { id: 3, src: "/assets/klaudia.webp" },
+  { id: 4, src: "/assets/mati.webp" },
+  { id: 5, src: "/assets/paniMarzenka.webp" },
+  { id: 6, src: "/assets/panJacek.webp" },
+  { id: 7, src: "/assets/panMaciek.webp" },
+  { id: 8, src: "/assets/tomek.webp" },
+  { id: 9, src: "/assets/adrian.webp" },
+  { id: 10, src: "/assets/darek.webp" },
+  { id: 11, src: "/assets/klaudia.webp" },
+  { id: 12, src: "/assets/mati.webp" },
+  { id: 13, src: "/assets/paniMarzenka.webp" },
+  { id: 14, src: "/assets/panJacek.webp" },
+  { id: 15, src: "/assets/panMaciek.webp" },
+  { id: 16, src: "/assets/tomek.webp" },
+];
+
+function shuffle(array) {
+  return array.sort(() => 0.5 - Math.random());
+}
